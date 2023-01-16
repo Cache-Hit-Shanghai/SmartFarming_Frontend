e559f3447c53775888fad04463f6fa477ec2eb81
black197 <dyeh68306@163.com>
2023-01-16 18:29:11 +0800

    update nextjs config

--- next.config.js
+++ next.config.js
@@ -67,6 +67,11 @@ module.exports = (phase) => {
         },
       ];
     },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
 
     // i18n,
   };
